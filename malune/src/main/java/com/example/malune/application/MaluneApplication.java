327662d06702c82545b768ed09b9b6df8d768143
BiaHinsching <biahc.bhcarvalho@gmail.com>
2026-07-21 18:44:59 -0300

    adiciona projeto malune e reorganiza pasta de bibliografia

---
+++ malune/src/main/java/com/example/malune/application/MaluneApplication.java
@@ -0,0 +1,13 @@
+package com.example.malune.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaluneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaluneApplication.class, args);
+    }
+
+}
---
+++ malune/src/test/java/com/example/malune/MaluneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.malune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaluneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
